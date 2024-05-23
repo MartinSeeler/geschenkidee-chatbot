66ed759e31a697ac03d0637ab30f7e607c61d6e3
Martin Seeler <developer@chasmo.de>
2024-05-23 20:54:48 +0200

    Add loading screen for chat

--- app/chat/loading.tsx
+++ app/chat/loading.tsx
@@ -1,4 +1,12 @@
+import { EmptyScreen } from "@/components/empty-screen";
+
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <div>Hello, I&apos;m loading</div>;
+  return (
+    <div className="group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]">
+      <div className={"pb-[200px] pt-4 md:pt-10"}>
+        <EmptyScreen />
+      </div>
+    </div>
+  );
 }
