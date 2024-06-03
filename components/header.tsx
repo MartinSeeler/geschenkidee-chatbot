0e375c5bca97824163013d9e8d3d6fe1f4f98d64
Martin Seeler <developer@chasmo.de>
2024-06-03 15:36:13 +0200

    Improve headline

--- components/header.tsx
+++ components/header.tsx
@@ -8,13 +8,12 @@ export default function Header() {
     <header className="inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center">
       <div className="mx-auto w-container max-w-3xl px-5 py-[110px] text-center lg:py-[150px]0">
         <h1 className="font-space text-5xl font-heading md:text-5xl lg:text-6xl text-pretty">
-          Finde das perfekte Geschenk in Sekunden
+          Finde das perfekte Geschenk in wenigen Sekunden!
         </h1>
         <p className="my-12 mt-8 text-lg font-normal leading-relaxed md:text-xl lg:text-2xl lg:leading-relaxed">
-          Schnell, personalisiert und ohne Anmeldung.
-          <br />
-          Mit deinem persönlichen KI-Berater findest du passende Geschenkideen
-          für jeden Anlass.
+          Vergiss das endlose Durchsuchen von Produktlisten. Unser KI-basierter
+          Geschenkeberater liefert dir sofort personalisierte Geschenkideen.
+          Einfach lostippen und loslegen!
         </p>
 
         <Button size="lg" theme="mint" asChild>
