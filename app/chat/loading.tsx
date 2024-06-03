3bab47fd1c32e62a3ff1e12d7c76303dcd7bb0d5
Martin Seeler <developer@chasmo.de>
2024-06-03 15:36:07 +0200

    Fix loading styling

--- app/chat/loading.tsx
+++ app/chat/loading.tsx
@@ -5,7 +5,9 @@ export default function Loading() {
   return (
     <div className="group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]">
       <div className={"pb-[200px] pt-4 md:pt-10"}>
-        <EmptyScreen />
+        <div className="mx-auto max-w-2xl px-4 flex flex-col gap-4">
+          <EmptyScreen />
+        </div>
       </div>
     </div>
   );
